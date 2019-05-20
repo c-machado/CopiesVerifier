e4e64289b8e1202e0d279507bcef690c77632215
Carolina Machado <machadoca@machadoca-macbookpro2.roam.corp.google.com>
2019-05-20 09:25:53 -0500

    config to include testng annotations

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -1,31 +1,47 @@
 package runner;
 
-//import org.testng.annotations.Test;
-import base.Homepage;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import base.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 
 public class TestRunner {
     //private TestNGCucumberRunner testNGCucumberRunner;
-    private Homepage home;
+    private static Homepage home;
+    private static SheetsQuickstart sheets;
 
-    /*@BeforeClass(alwaysRun = true)
+    @BeforeClass
     public void setUpClass() throws Exception {
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-    }*/
+        //testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+        System.out.println("before");
+    }
 
-    //@Test
+    @Test
     public void feature() {
         //testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-        home = new Homepage();
-        home.setUp();
+//        home = new Homepage();
+//        home.setUp();
+        System.out.println("before");
     }
 
     /*@DataProvider
     public Object[][] features() {
         return testNGCucumberRunner.provideFeatures();
+    }*/
+
+    @AfterClass
+    public void tearDownClass() throws Exception {
+        //testNGCucumberRunner.finish();
+        System.out.println("after");
     }
 
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() throws Exception {
-        testNGCucumberRunner.finish();
-    }*/
+    public static void main(String args[]) throws IOException, GeneralSecurityException {
+        //home = new Homepage();
+        //home.setUp();
+        sheets.authenticate();
+    }
 }
