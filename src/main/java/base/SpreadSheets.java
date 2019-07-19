ff25a5d89a18808d23bd268d29a7e04c22827a4e
Carolina Machado <machadoca@machadoca-macbookpro2.roam.corp.google.com>
2019-07-19 13:55:15 -0500

    update in structure

---
+++ src/main/java/base/SpreadSheets.java
@@ -0,0 +1,120 @@
+package base;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.*;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class SpreadSheets {
+
+    private final String APPLICATION_NAME = "Google SpreadSheets API Java Quickstart";
+    private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private final String TOKENS_DIRECTORY_PATH = "tokens";
+    //private Browser copiesValidation = new Browser();
+    private String spreadsheetId;
+    private com.google.api.services.sheets.v4.Sheets service;
+    private final String valueInputOption = "ROW";
+
+    public SpreadSheets(String _spreadsheetId){
+        this.spreadsheetId = _spreadsheetId;
+    }
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+    private final String CREDENTIALS_FILE_PATH = "/credentials_old.json";
+
+
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials_old.json file cannot be found.
+     */
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = SpreadSheets.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    private Sheets getCredentials3(){
+        AppIdentityCredential credential =
+                new AppIdentityCredential(
+                        Collections.singletonList(SheetsScopes.SPREADSHEETS));
+        return new  com.google.api.services.sheets.v4.Sheets.Builder(new UrlFetchTransport(),
+                JacksonFactory.getDefaultInstance(),
+                credential)
+                .build();
+    }
+
+    /**
+     * Prints the names and majors of students in a sample spreadsheet:
+     * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+     */
+
+    public void authenticate() throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        this.service = new com.google.api.services.sheets.v4.Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        //service = this.getCredentials();
+    }
+
+    public List<List<Object>> getValues(String _range) throws IOException {
+        ValueRange response = this.service.spreadsheets().values()
+                .get(this.spreadsheetId, _range)
+                .execute();
+        return response.getValues();
+    }
+
+    public void appendValues(String _range, List<List<Object>> _values)
+            throws IOException {
+        ValueRange body = new ValueRange().setValues(_values);
+        AppendValuesResponse result =
+                service.spreadsheets().values()
+                        .append(this.spreadsheetId, _range, body)
+                        .setValueInputOption(this.valueInputOption)
+                        .execute();
+        System.out.printf("%d cells appended.", result.getUpdates().getUpdatedCells());
+
+    }
+}
+
