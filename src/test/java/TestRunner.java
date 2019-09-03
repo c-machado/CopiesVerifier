586ca7bd9c48ccdb82fb335a96361f2af5053fba
c-machado <33238142+c-machado@users.noreply.github.com>
2019-09-02 21:58:34 -0500

    Feature/selectors sheet (#8)
    * test to run
    
    * removing compiled files from repo, trying different build config, adding chromedriver
    
    * adding missing dependencies
    
    * checking different method to see if it works
    
    * config to include testng annotations
    
    * fixing gitignore conflicts
    
    * create main class to test the sheets auth process
    
    * initial configuration
    
    * changing iteration method over rows to avoid search null indexes
    
    * adding comments
    
    * empty row validation: iterator vs. for
    
    * search the element based on the selector
    
    * get copies based on the src tag
    
    * cleanning some unused code
    
    * hero module selectors
    
    * video module selectors
    
    * missing selectors for the home page
    
    * clean code
    
    * update in structure
    
    * fix in authentication, and fix to write a file with the errors
    
    * selectors for all pages within the assistant site
    
    * iterate whole assistant copies sheet

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,6 @@
 
 import base.Browser;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import base.SpreadSheets;
 import java.io.IOException;
@@ -18,83 +18,119 @@ public class TestRunner {
     private final String spreadsheetIdCopies = "1okfhWpOVqNP6zqm6hqNJVfUygqsrtgn-Zg4B-wlAwgo";
     private final String spreadSheetIdErrors = "17Q49zfLzpkN483hxixKZPopUQUBVwkplAxssoWx63sA";
 
-    //private final String spreadSheetIdErrors = "1sXL9zoHI-xdbQj--TGzcaGPu_zL01kZuFKcySQyLgsM";
-    private final String rangeInput = "Homepage!A3:C89";
-
-//    private final String rangePhonesInput = "Phones!A3:C170";
-    private final String rangeSpeakersInput = "Smart Displays!A3:C80";
-//    private final String rangeOutput = "Homepage!A4:C4";
-//    private final String rangePhonesOutput = "Phones!A4:C112";
-    private final String rangeSpeakersOutput = "Smart Displays!A4:C100";
-
-    private final String rangeOutput = "Homepage!A4:C4";
-
-
     private Browser browser;
     private final String url = "https://assistant.google.com/platforms/displays/";
+    private final String base_URL = "https://assistant.google.com";
+
+    public List<String> getSourceSheet(){
+        List<String> sheets = new ArrayList<String>();
+        sheets.add("Homepage!A3:C89");
+        sheets.add("Phones!A3:C163");
+        sheets.add("Speakers!A3:C202");
+        sheets.add("Cars!A3:C56");
+        sheets.add("TVs!A3:C64");
+        sheets.add("Laptops&Tablets!A3:C78");
+        sheets.add("Wearables!A3:C98");
+        sheets.add("More Devices (Smart Clock)!A3:C53");
+        sheets.add("Smart Displays!A3:C57");
+        return sheets;
+    }
+
+    public String getURL(String _range){
+        String url = _range.substring(0,_range.indexOf('!'));
+        System.out.println("carola "+url);
+        String complete_URL="";
+        switch (url){
+            case "Homepage":
+                complete_URL = base_URL;
+                break;
+            case "Phones":
+                complete_URL = base_URL + "/platforms/phones";
+                break;
+            case "Speakers":
+                complete_URL = base_URL + "/platforms/speakers";
+                break;
+            case "Cars":
+                complete_URL = base_URL + "/platforms/cars";
+                break;
+            case "TVs":
+                complete_URL = base_URL + "/platforms/tv";
+                break;
+            case "Laptops&Tablets":
+                complete_URL = base_URL + "/platforms/laptops";
+                break;
+            case "Wearables":
+                complete_URL = base_URL + "/platforms/wearables";
+                break;
+            case "More Devices (Smart Clock)":
+                complete_URL = base_URL + "/platforms/devices";
+                break;
+            case "Smart Displays":
+                complete_URL = base_URL + "/platforms/displays";
+                break;
+            default:
+                System.out.println("Something went wrong");
+        }
+
+        return  complete_URL;
+    }
 
     @Test
-    public void feature() throws IOException, GeneralSecurityException {
+    public void feature() {
         try {
 
+            List<String> sheetsCopies = new ArrayList<String>();
+            sheetsCopies = getSourceSheet();
             sourceSheet = new SpreadSheets(spreadsheetIdCopies);
             errorSheet = new SpreadSheets(spreadSheetIdErrors);
-            browser = new Browser(url);
-            /*sourceSheet.authenticate(spreadsheetIdCopies, rangeInput);
-            errorSheet.authenticate(spreadSheetIdErrors, rangeOutput);        */
-            sourceSheet.authenticate(spreadsheetIdCopies,rangeSpeakersInput);
-            errorSheet.authenticate(spreadSheetIdErrors,rangeSpeakersOutput);
-            boolean copiesMatched;
-            List<List<Object>> copiesOrigin = sourceSheet.getValues(rangeSpeakersInput);
-            //List<List<Object>> copiesOrigin = sourceSheet.getValues(rangeInput);
+            for(String sheet: sheetsCopies) {
 
-            String selector = "", copyOnSheet = "", copyOnSite = "";
-            List rows = new ArrayList();
-            for (int rowNumber = 0; rowNumber < copiesOrigin.size(); rowNumber++) {
-                copiesMatched = false;
-                List<Object> row = copiesOrigin.get(rowNumber);
+                String _url = getURL(sheet);
+                browser = new Browser(_url);
 
-                if (row.size() > 0) {
-                    selector = row.get(0).toString();
-                    if (row.size() > 2) {
-                        copyOnSheet = row.get(2).toString().trim();
+//                sourceSheet.authenticate(spreadsheetIdCopies, rangeInput);
+//                errorSheet.authenticate(spreadSheetIdErrors, rangeOutput);
+                sourceSheet.authenticate(spreadsheetIdCopies, sheet);
+                errorSheet.authenticate(spreadSheetIdErrors, sheet);
+                boolean copiesMatched;
+                List<List<Object>> copiesOrigin = sourceSheet.getValues(sheet);
+
+                String selector = "", copyOnSheet = "", copyOnSite = "";
+                List rows = new ArrayList();
+                for (int rowNumber = 0; rowNumber < copiesOrigin.size(); rowNumber++) {
+                    copiesMatched = false;
+                    List<Object> row = copiesOrigin.get(rowNumber);
+                    if (row.size() > 0) {
+                        selector = row.get(0).toString();
+                        if (row.size() > 2) {
+                            copyOnSheet = row.get(2).toString().trim();
+                        }
                     }
-                }
+                    if (!selector.isEmpty()) {
+                        copyOnSite = this.browser.getCopy(selector);
+                        copiesMatched = copyOnSheet.equals(copyOnSite);
+                        if (copiesMatched == false) {
+                            //save in the error file
+                            List column = new ArrayList();
+                            column.add(selector);
+                            column.add(copyOnSite);
+                            column.add(copyOnSheet);
+                            rows.add(column);
+                        }
 
-                if (!selector.isEmpty()) {
-                    copyOnSite = this.browser.getCopy(selector);
-                    copiesMatched = copyOnSheet.equals(copyOnSite);
-                    if (copiesMatched == false) {
-                        //save in the error file
-
-                        List column = new ArrayList();
-                        column.add(selector);
-                        column.add(copyOnSite);
-                        column.add(copyOnSheet);
-                        rows.add(column);
                     }
+                    System.out.println("----------");
+                    System.out.println("copies \n" +
+                            "\trowNumber > " + rowNumber + "\n" +
+                            "\tSelector > " + selector + "\n" +
+                            "\tCopy > " + copyOnSheet + "\n" +
+                            "\tCopyOnSite > " + copyOnSite + "\n" +
+                            "\tCopiesMatched > " + copiesMatched);
+                    System.out.println("----------");
                 }
+                errorSheet.appendValues(sheet, rows);
+            }
 
-                System.out.println("----------");
-                System.out.println("copies \n" +
-                        "\trowNumber > " + rowNumber + "\n" +
-                        "\tSelector > " + selector + "\n" +
-                        "\tCopy > " + copyOnSheet + "\n" +
-                        "\tCopyOnSite > " + copyOnSite + "\n" +
-                        "\tCopiesMatched > " + copiesMatched);
-                System.out.println("----------");
-
-           }
-
-                errorSheet.appendValues(rangeOutput, rows);
-
-
-                //System.out.println("copies " + copiesOrigin.get(0).get(2));
-
-            /*List<List<Object>> spreadSheetValues = sheets.authenticate(spreadsheetIdCopies,range);
-            List<List<Object>> rows = sheets.validateCopies(spreadSheetValues);
-            //sheets.authenticate(spreadSheetIdErrors,range);
-            sheets.appendValues(spreadSheetIdErrors,range,"ROW", rows);*/
 
         } catch(IOException e){
             e.printStackTrace();
@@ -104,7 +140,8 @@ public class TestRunner {
 
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterTest
+
     public void tearDownClass() throws Exception {
        browser.driver.quit();
     }
